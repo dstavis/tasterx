de3622e4f07d7a83061aaa10292e9d69659f192e
Anna Spitz <aspitzemail@gmail.com>
2022-09-10 08:56:58 -0500

    Add Search form

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,3 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const Search = (searchForShow) => {
+    const [showName, setShowName] = useState('');
+
+    return (
+        <form>
+            <input 
+                type='text'
+                name='showName'
+                value={showName}
+                onChange={(event) => setShowName(event.target.value)}
+            />
+
+            <button className='search-button' onClick={(event) => searchForShow(showName)}>Search</button>
+        </form>
+    )
+}
 
 export default Search
