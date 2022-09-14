2d277a37f6e4f03fbc53282787e1c7e0a69b6fca
Eddie Rodriguez <edwardjrodriguez17@gmail.com>
2022-09-14 16:13:46 -0600

    Begin styling prescription component

--- src/components/Prescription/Prescription.js
+++ src/components/Prescription/Prescription.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import './Prescription.css';
+import rxLogo from '../../assets/RX-logo.svg';
 import { getShowById, getPrescription } from '../../api-calls/api-calls';
 
 const Prescription = () => {
@@ -11,8 +12,8 @@ const Prescription = () => {
   useEffect(() => {
     getPrescription(id)
       .then(data => {
-        setPrescription(data.data);
-        getShowById(data.data.showID)
+        setPrescription(data.prescription);
+        getShowById(data.prescription.showID)
           .then(data => {
             const { name, officialSite, image } = data;
             setShow({
@@ -27,11 +28,18 @@ const Prescription = () => {
   },[])
 
   return (
-    <div>
-      <p>{show.name}</p>
-      <img src={show.image} alt='show poster'/>
-      <a href={show.officialSite}>Official TV show website</a>
-      <p>{prescription.message}</p>
+    <div className='script-container'>
+      <div className='top-container'> 
+        <div className='logo-and-prompt'>
+          <img className='script-logo' src={rxLogo} alt='rx logo' />
+          <p className='to-be-prescribed-message'>Show to be prescribed:</p>
+        </div>
+        <p >{show.name}</p>
+        <a href={show.officialSite}>Official TV show website</a>
+      </div>
+        <img className='script-show-poster'  src={show.image} alt='show poster'/>
+      <div>
+      </div>
     </div>
   );
 }
