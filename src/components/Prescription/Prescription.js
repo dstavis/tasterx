efb32b59d6abc3f584bef64c72212ecd8f96c63f
Eddie Rodriguez <edwardjrodriguez17@gmail.com>
2022-09-14 20:45:01 -0600

    Phase 2 styling presecription component

--- src/components/Prescription/Prescription.js
+++ src/components/Prescription/Prescription.js
@@ -27,18 +27,22 @@ const Prescription = () => {
         .catch(error => console.log(error));
   },[])
 
+  console.log(prescription.message)
   return (
     <div className='script-container'>
       <div className='top-container'> 
         <div className='logo-and-prompt'>
           <img className='script-logo' src={rxLogo} alt='rx logo' />
-          <p className='to-be-prescribed-message'>Show to be prescribed:</p>
+          <p className='to-be-prescribed'>Show to be prescribed:</p>
         </div>
-        <p >{show.name}</p>
+        <p className='script-show-name'>{show.name}</p>
         <a href={show.officialSite}>Official TV show website</a>
       </div>
-        <img className='script-show-poster'  src={show.image} alt='show poster'/>
-      <div>
+        <img className='script-show-poster' src={show.image} alt='show poster'/>
+      <div className='bottom-container'>
+          <p>Write a message for the prescription:</p>
+          <p>{prescription.message}</p>
+    
       </div>
     </div>
   );
